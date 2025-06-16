5f07b3af07754d0e51d61e5886f79ff85bede046
Alveroth <richardjohnwoods@gmail.com>
2025-06-16 00:54:51 -0500

    update app and index for dummy data

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/scout', methods=['POST'])
+def scout():
+    player_name = request.form.get('player_name')
+    
+    # Dummy data just to test form pipeline
+    report = {
+        "Player": player_name,
+        "MMR": "4,250",
+        "Favorite Hero": "Earth Spirit",
+        "Win Rate": "56%",
+        "Role": "Roaming Support"
+    }
+
+    return render_template('index.html', report=report)
